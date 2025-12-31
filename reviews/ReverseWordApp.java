dfcd1bb640371c2714faf6993c85f4810ef3ec2e
nitishkumar124 <ns8231001@gmail.com>
2025-12-31 15:34:38 +0530

    [Nitish Kumar] fix . review01: reverse-word-app use case 2

--- reviews/ReverseWordApp.java
+++ reviews/ReverseWordApp.java
@@ -6,11 +6,30 @@ public class ReverseWordApp {
   static String removeExtraSpaces(String text) {
     return text.trim();
   }
+
+  // UC2
+  static String[] separateWords(String trimmedText) {
+    return trimmedText.split(" ");
+  }
+
+  // method to display words
+  static void displayWords(String[] words) {
+    System.out.println("\nwords:");
+    for (String word : words){
+      System.out.println(word);
+    }
+    System.out.println();
+  }
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String text = sc.nextLine();
 
     String trimmedText = removeExtraSpaces(text);
     System.out.println("text after extra spaces removed: " + trimmedText);
+
+    String[] words = separateWords(trimmedText);
+    displayWords(words);
+
   }
 }
