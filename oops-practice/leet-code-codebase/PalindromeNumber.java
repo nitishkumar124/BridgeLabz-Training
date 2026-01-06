ab4c36320fa7c895584ecd877b374fcdd833c502
nitishkumar124 <ns8231001@gmail.com>
2026-01-06 16:10:02 +0530

    [Nitish Kumar] add . submission: leet-code problems

---
+++ oops-practice/leet-code-codebase/PalindromeNumber.java
@@ -0,0 +1,27 @@
+package week2;
+
+import java.util.*;
+
+public class PalindromeNumber {
+	public static boolean isPalindrome(int x) {
+		int num = 0;
+		int temp = x;		
+		while (temp > 0) {
+			num += temp % 10;
+			temp /= 10;
+			num *= 10;			
+		}
+		num /= 10;
+		System.out.println(num);
+		return x == num;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the value of x : ");
+		int x = sc.nextInt();
+		boolean result = isPalindrome(x);
+		System.out.println("Is Palindrome: " + result);
+		sc.close();
+	}
+}
